ee7ff30a50166ac12df6a8fa81556daa28959fae
Jirow13 <67016716+Jirow13@users.noreply.github.com>
2020-08-10 18:11:58 -0400

    Initial
    Initial commit

---
+++ Main.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using System;
+using System.Text;
+using System.Windows.Forms;
+using TaleWorlds.MountAndBlade;
+
+namespace ExitSettlement
+{
+	public class ExceptionHandler
+	{
+		public static string ToString(Exception ex)
+		{
+			return GetString(ex);
+		}
+
+		private static string GetString(Exception ex)
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+
+			GetStringRecursive(ex, stringBuilder);
+
+			stringBuilder.AppendLine();
+			stringBuilder.AppendLine("Stack trace: ");
+			stringBuilder.AppendLine(ex.StackTrace);
+
+			return stringBuilder.ToString();
+		}
+
+		private static void GetStringRecursive(Exception ex, StringBuilder sb)
+		{
+			sb.AppendLine(ex.GetType().Name + ": ");
+			sb.AppendLine(ex.Message);
+			if (ex.InnerException != null)
+			{
+				sb.AppendLine();
+				GetStringRecursive(ex.InnerException, sb);
+			}
+		}
+	}
+
+	public class ExitSettlementSubModule : MBSubModuleBase
+	{
+		protected override void OnSubModuleLoad()
+		{
+			try
+			{
+				Harmony harmonyPatch = new Harmony("bannerlord.exitsettlement");
+				harmonyPatch.PatchAll();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Failed hooking settlement exit on click code\n\n" + ExceptionHandler.ToString(ex));
+			}
+		}
+	}
+}
---
+++ Settlement Hook.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using SandBox.View.Map;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameMenus;
+
+namespace ExitSettlement
+{
+	[HarmonyPatch(typeof(MapScreen), "HandleLeftMouseButtonClick")]
+	public class MapScreenHandleLeftMouseButtonClickPatch
+	{
+		private static AccessTools.FieldRef<MapScreen, MapState> mapState = AccessTools.FieldRefAccess<MapScreen, MapState>("_mapState");
+
+		private static void Prefix(MapScreen __instance, PathFaceRecord mouseOverFaceIndex)
+		{
+			if (!mouseOverFaceIndex.IsValid())
+				return;
+
+			if (PlayerSiege.Current != null || MobileParty.MainParty.SiegeEvent != null)
+				return;
+
+			if (mapState(__instance) != null && mapState(__instance).AtMenu)
+			{
+				Settlement currentSettlement = MobileParty.MainParty.CurrentSettlement;
+				if (currentSettlement != null && (currentSettlement.IsCastle || currentSettlement.IsFortification || currentSettlement.IsTown || currentSettlement.IsVillage))
+				{
+					PlayerEncounter.LeaveSettlement();
+					GameMenu.ExitToLast();
+					PlayerEncounter.Finish(true);
+				}
+				else
+				if (MobileParty.MainParty.TargetSettlement != null)
+				{
+					PlayerEncounter.Finish(true);
+				}
+			}
+		}
+	}
+}
